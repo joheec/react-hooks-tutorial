a0e55b55d06912b22458e15da3d198ffb5d6cf38
johee <johchung@microsoft.com>
2021-11-10 15:05:26 -0600

    Setup initial layout

--- src/App.js
+++ src/App.js
@@ -6,18 +6,31 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Hooks</h1>
+        <h3>By Johee Chung</h3>
       </header>
+      <div id="content">
+        <div className="text-center">
+          <a
+            className="App-link"
+            href="https://reactjs.org/docs/hooks-intro.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Official React Hooks Docs
+          </a>
+        </div>
+        <div className="text-center">
+          <a
+            className="App-link"
+            href="https://beta.reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Beta React Docs
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
