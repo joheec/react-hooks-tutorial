8369c8d8df596b170c6d290b835462d1b57e3fab
johee <johchung@microsoft.com>
2021-11-11 00:17:16 -0600

    Second sample for useRef

--- src/components/UseRef/index.js
+++ src/components/UseRef/index.js
@@ -1,4 +1,4 @@
-import { useState, useMemo, useRef } from 'react';
+import { useState, useMemo, useRef, useEffect } from 'react';
 import Tab from '../Tab';
 
 const UseRef = () => {
@@ -55,8 +55,21 @@ const UseRef = () => {
 		}
 	};
 
+	const [isPink, setIsPink] = useState(true);
+	const refBoxEl = useRef();
+
+	useEffect(() => {
+		refBoxEl.current.style.backgroundColor = "pink";
+	}, []);
+
+	const refSwitchColor = () => {
+		const color = refBoxEl.current.style.backgroundColor;
+		refBoxEl.current.style.backgroundColor = color === "pink" ? "white" : "pink";
+	};
+
 	return <div>
 		<h2>UseRef</h2>
+		<h3>Sample 1</h3>
 		<p>Updating value is causing an unnecessary re-render</p>
 		<code>
 			1&nbsp; | const [value, setValue] = useState(0);<br />
@@ -110,6 +123,38 @@ const UseRef = () => {
 			<button onClick={() => setRefOperator("x")}>multiply</button>
 			<button onClick={() => setRefOperator("/")}>divide</button>
 			<button onClick={() => setRefOperator("+")}>add</button>
+		</div>
+		<h3>Sample 2</h3>
+		<p>Logic in the inline styles require re-rendering from useState to visualize changes. This will call the render, generate the virtual DOM, and compare the DOMs. All this for a style change.</p>
+		<code>
+			1 | const [isPink, setIsPink] = true;<br />
+			2 | <br />
+			3 | return {"<div>"}<br />
+			4 | <Tab />{"<div style={{ backgroundColor: isPink ? 'pink' : 'white' }}></div>"}<br />
+			5 | <Tab />{"<button onClick={() => setIsPink(!isPink)}>Switch Color</button>"}<br />
+			6 | {"</div>;"}<br />
+		</code>
+		<div className="sample">
+			<div style={{ height: '50px', width: '50px', backgroundColor: isPink ? 'pink' : 'white' }}></div>
+			<button onClick={() => setIsPink(!isPink)}>Switch Color</button>
+		</div>
+		<p>Using useRef instead of useState prevents re-rendering due to update. By assigning the returned object from useRef to the element's ref attribute, changes will be registered as simple style changes.</p>
+		<code>
+			1 &nbsp;| const boxEl = useRef();<br />
+			2 &nbsp;|<br />
+			3 &nbsp;| {"const toggleColor = () => {"}<br />
+			4 &nbsp;| <Tab />const color = boxEl.current.style.backgroundColor;<br />
+			5 &nbsp;| <Tab />boxEl.current.style.backgroundColor = color === "pink" ? "white" : "pink";<br />
+			6 &nbsp;| {"}"}<br />
+			7 &nbsp;|<br />
+			8 &nbsp;| return {"<div>"}<br />
+			9 &nbsp;| <Tab />{"<div ref={boxEl}></div>"}<br />
+			10 | <Tab />{"<button onClick={() => toggleColor}>Switch Color</button>"}<br />
+			11 | {"</div>;"}<br />
+		</code>
+		<div className="sample">
+			<div ref={refBoxEl} style={{ height: '50px', width: '50px' }}></div>
+			<button onClick={refSwitchColor}>Switch Color</button>
 		</div>
 	</div>;
 }
